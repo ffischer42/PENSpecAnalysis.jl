22a6eb75b744ca859b64b33c5899f9c7250f3c9e
Connor Hayward <connor.hayward@hotmail.co.uk>
2018-06-26 11:13:13 +0200

    Test pm sign

--- src/fit_spec.jl
+++ src/fit_spec.jl
@@ -5,7 +5,7 @@ function peakfit(xaxis,yaxis)
     p = polyfit(float.(xaxis[max[2]-5:max[2]+5]),float.(yaxis[max[2]-5:max[2]+5]),2)
     c = coeffs(p)
     fit_function(x) = c[1]+c[2]x+c[3]x^2  # [f(x) = f(x) + c[iter]*x^(iter-1) for iter in eachindex(c)]
-    peak = roots(polyder(p,1))[1] \pm   (xaxis[end]-xaxis[1])/length(xaxis)
+    peak = roots(polyder(p,1))[1] ± (xaxis[end]-xaxis[1])/length(xaxis)
     return peak, fit_function, chisquare(xaxis[max[2]-5:max[2]+5], yaxis[max[2]-5:max[2]+5], p),p
 end
 
@@ -14,7 +14,7 @@ function peakfit(xaxis,yaxis,range)
     p = polyfit(float.(xaxis[max[2]-range:max[2]+range]),float.(yaxis[max[2]-range:max[2]+range]),2)
     c = coeffs(p)
     fit_function(x) = c[1]+c[2]x+c[3]x^2  # [f(x) = f(x) + c[iter]*x^(iter-1) for iter in eachindex(c)]
-    peak = roots(polyder(p,1))[1] \pm   (xaxis[end]-xaxis[1])/length(xaxis)
+    peak = roots(polyder(p,1))[1] ± (xaxis[end]-xaxis[1])/length(xaxis)
     diffstring = @sprintf("%0.2f",r[1]-xaxis[indmax(yaxis)])
     if range == 15
         println("Difference [fit - data] = $diffstring nm")
@@ -27,7 +27,7 @@ function peakfit(xaxis,yaxis,range,string::String)
     p = polyfit(float.(xaxis[max[2]-15:max[2]+7]),float.(yaxis[max[2]-15:max[2]+7]),2)
     c = coeffs(p)
     fit_function(x) = c[1]+c[2]x+c[3]x^2
-    peak = roots(polyder(p,1))[1] \pm   (xaxis[end]-xaxis[1])/length(xaxis)
+    peak = roots(polyder(p,1))[1] ± (xaxis[end]-xaxis[1])/length(xaxis)
     diffstring = @sprintf("%0.2f",r[1]-xaxis[indmax(yaxis)])
     println("Difference [fit - data] = $diffstring nm")
     return peak, fit_function, chisquare(xaxis[max[2]-15:max[2]+7], yaxis[max[2]-15:max[2]+7], p),p
