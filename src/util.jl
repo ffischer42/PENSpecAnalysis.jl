110a43cde0338b31e751c73a3c2ac9567e14dd03
Connor Hayward <connor.hayward@hotmail.co.uk>
2018-06-26 15:01:31 +0200

    Move prepare_dir to util

--- src/util.jl
+++ src/util.jl
@@ -17,10 +17,26 @@ function find_index(h::Histogram,value)
     index = find_index(x,value)
     return index
 end
-export find_index
 
 # Include Measurements support
 function find_value(h::Histogram, value)
     h.weights[find_index(h,value)] ± 1 / sqrt(h.weights[find_index(h,value)])
 end
 export find_value
+
+function prepare_dir(filepath::String, pattern="")
+    data_dir = "/remote/ceph/group/gedet/data/pen/2018/$filepath"
+    data_list = glob("*$pattern*",data_dir)
+    data_dict = OrderedDict()
+    start_cha = length(data_dir)+1
+    for iter in eachindex(data_list)
+        if contains(data_list[iter],".")
+            data_dict[data_list[iter][start_cha:findlast(data_list[iter],'.')-1]]=data_list[iter]
+        else
+            data_dict[data_list[iter][start_cha:end]]=data_list[iter]
+        end
+    end
+    return data_dict
+end
+
+export prepare_dir
