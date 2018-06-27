61ae42786b3d955c7c8324d83f3aaaed93b3df51
Connor Hayward <connor.hayward@hotmail.co.uk>
2018-06-27 15:49:57 +0200

    Added support to change range used for fit

--- src/fit_spec.jl
+++ src/fit_spec.jl
@@ -16,15 +16,16 @@ Fits second order polynomial to mapping x -> y around maximal y-value.
 - 'chisquare::Float64' chi squared of fit to input data.
 - 'p::Polynomial' polynomial computed, alternate form of fit_function.
 """
-function peakfit(xaxis,yaxis)
+function peakfit(xaxis,yaxis; range=10)
+    range = range / 2
     max =  findmax(yaxis)
-    p = polyfit(float.(xaxis[max[2]-5:max[2]+5]),float.(yaxis[max[2]-5:max[2]+5]),2)
+    p = polyfit(float.(xaxis[max[2]-range:max[2]+range]),float.(yaxis[max[2]-range:max[2]+range]),2)
     c = coeffs(p)
     fit_function(x) = c[1]+c[2]x+c[3]x^2  # [f(x) = f(x) + c[iter]*x^(iter-1) for iter in eachindex(c)]
     peak = roots(polyder(p,1))[1] ± (xaxis[end]-xaxis[1])/length(xaxis)
     return peak, fit_function, chisquare(xaxis[max[2]-5:max[2]+5], yaxis[max[2]-5:max[2]+5], p),p
 end
-
+"""
 function peakfit(xaxis,yaxis,range)
     max =  findmax(yaxis)
     p = polyfit(float.(xaxis[max[2]-range:max[2]+range]),float.(yaxis[max[2]-range:max[2]+range]),2)
@@ -37,6 +38,7 @@ function peakfit(xaxis,yaxis,range)
     end
     return peak, fit_function, chisquare(xaxis[max[2]-range:max[2]+range], yaxis[max[2]-range:max[2]+range], p),p
 end
+"""
 
 function peakfit(xaxis,yaxis,range,string::String)
     max =  findmax(yaxis)
