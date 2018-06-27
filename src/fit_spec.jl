c66f094919102f34048a4f28be4d67066f6d9e12
Connor Hayward <connor.hayward@hotmail.co.uk>
2018-06-27 16:19:09 +0200

    Resolve error in accessing invalid bins

--- src/fit_spec.jl
+++ src/fit_spec.jl
@@ -19,11 +19,11 @@ Fits second order polynomial to mapping x -> y around maximal y-value.
 function peakfit(xaxis,yaxis; range=10)
     range = range / 2
     max =  findmax(yaxis)
-    p = polyfit(float.(xaxis[max[2]-range:max[2]+range]),float.(yaxis[max[2]-range:max[2]+range]),2)
+     p = polyfit(float.(xaxis[max[2]]-range):float.(xaxis[max[2]]+range),float.(yaxis[max[2]]-range):float.(yaxis[max[2]]+range),2)
     c = coeffs(p)
     fit_function(x) = c[1]+c[2]x+c[3]x^2  # [f(x) = f(x) + c[iter]*x^(iter-1) for iter in eachindex(c)]
     peak = roots(polyder(p,1))[1] ± (xaxis[end]-xaxis[1])/length(xaxis)
-    return peak, fit_function, chisquare(xaxis[max[2]-5:max[2]+5], yaxis[max[2]-5:max[2]+5], p),p
+    return peak, fit_function, chisquare(xaxis[max[2]]-range):float.(xaxis[max[2]]+range, yaxis[max[2]]-range):float.(yaxis[max[2]]+range, p),p
 end
 
 # function peakfit(xaxis,yaxis,range)
