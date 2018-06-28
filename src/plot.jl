76d643c6f4bd8a7f9ab7daa8ee440d03dc24aeb8
Connor Hayward <connor.hayward@hotmail.co.uk>
2018-06-28 09:43:58 +0200

    Added range control to plotting functions

--- src/plot.jl
+++ src/plot.jl
@@ -8,14 +8,14 @@ Produce plots of each spectra in given Dict.
 
 Plot can optionally contain peak wavelength and chi2 of fit.
 """
-function plot_emission_spectra(dict::OrderedDict,fit=false)
+function plot_emission_spectra(dict::OrderedDict,fit=false;range=10)
     plt = plot(bg=:white)
     for key in keys(dict)
         h = read_spec_data(dict[key])
         plot!(plt, h,label="$key",st=:step)
 
         if fit
-            fitting = peakfit(h)
+            fitting = peakfit(h,range=range)
             peak_val = Measurements.value(fitting[1])
             peak_unc = Measurements.uncertainty(fitting[1])
             scatter!(plt,[peak_val],[Measurements.value(fitting[2](fitting[1]))],xlims=[h.edges[1][1]+10,h.edges[1][end]-10],label="$(@sprintf("%0.2f",peak_val)) ± $(@sprintf("%0.2f",peak_unc)) nm")
@@ -38,7 +38,7 @@ Plot can optionally contain peak wavelength and chi2 of fit.
 # Arguements
 - 'mode' = normalization setting to be used. If height, histogram normallized to highest weight. If area, normalized so norm(h)=1.
 """
-function plot_norm_emission_spectra(dict::OrderedDict,fit=false; mode="height")
+function plot_norm_emission_spectra(dict::OrderedDict,fit=false; mode="height", range=10)
     plt = plot(bg=:white)
     for key in keys(dict)
         h = read_spec_data(dict[key])
@@ -53,7 +53,7 @@ function plot_norm_emission_spectra(dict::OrderedDict,fit=false; mode="height")
 
         plot!(plt, h,label="$key",st=:step)
         if fit
-            fitting = peakfit(h)
+            fitting = peakfit(h,range=range)
             peak_val = Measurements.value(fitting[1])
             peak_unc = Measurements.uncertainty(fitting[1])
             scatter!(plt,[peak_val],[Measurements.value(fitting[2](fitting[1]))],xlims=[h.edges[1][1]+10,h.edges[1][end]-10],label="$(@sprintf("%0.2f",peak_val)) ± $(@sprintf("%0.2f",peak_unc)) nm")
